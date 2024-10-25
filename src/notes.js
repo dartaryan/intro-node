0df573e6fd78853083849b271d09856df8d352e7
ben.akiva <ben.akiva@passportcard.co.il>
2024-10-25 23:18:58 +0300

    read and delete notes

--- src/notes.js
+++ src/notes.js
@@ -1,4 +1,4 @@
-import { getDB, insert } from './db.js'
+import { getDB, insert, saveDB } from './db.js'
 
 export const newNote = async (note, tags) => {
   const newNote = { content: note, tags: tags, id: Date.now() }
@@ -9,4 +9,25 @@ export const newNote = async (note, tags) => {
 export const getAlNotes = async () => {
   const { notes } = await getDB()
   return notes
+}
+
+export const findNotes = async (filter) => {
+  const { notes } = await getDB()
+  return notes.filter(
+    note => note.content.toLowerCase().includes(filter.toLowerCase()))
+}
+
+export const removeNote = async (id) => {
+  const notes = await getAlNotes()
+  const match = notes.find(note => note.id === id)
+
+  if (match) {
+    const newNotes = notes.filter(note => note.id !== id)
+    await saveDB({ notes: newNotes })
+    return id
+  }
+}
+
+export const removeAllNotes = async () => {
+  await saveDB({ notes: [] })
 }
