79d4c9bf9137892dec0f68e0c65d2006d26b7a4d
ben.akiva <ben.akiva@passportcard.co.il>
2024-10-25 19:03:42 +0300

    Initial commit

---
+++ src/command.js
@@ -0,0 +1,13 @@
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+yargs(hideBin(process.argv)).
+  command('new <note>', 'Create a new note', (yargs) => {
+    return yargs.positional('note', {
+      type: 'string', description: 'The content of the note to create',
+    })
+
+  }, (argv) => {console.log(argv.note)}).option('tags', {
+  alias: 't', type: 'array', description: 'Tags for the note',
+
+}).demandCommand(1).parse()
